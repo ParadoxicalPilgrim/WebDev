d816ed5bf8d16f7244b4312eb35bc5525629ef34
ParadoxicalPilgrim <otherusesformyself98@gmail.com>
2025-07-23 17:53:05 +0530

    SERVER WITH EXPRESS JS
    I implement Express JS into creating a basic server that exchanges data through Postman API.

--- index.js
+++ index.js
@@ -1,30 +1,23 @@
-const fs=require('fs');
-const mongoose=require('mongodb');
 const express=require('express');
 const exp=express();
-
-const users=require('./MOCK_DATA.json');
-
-exp.use(express.json());
-exp.use(express.urlencoded({extended:false}));
+const fs=require('fs');
 
 exp.get('/',(req,res)=>{
-    res.send('Homepage');
-});
 
-exp.get('/users',(req,res)=>{
-    return res.json(users);
-});
-
-exp.post('/users',(req,res)=>{
-    let body=req.body;
-    console.log(body);
-    users.push({id:users.length+1,...body});
-
-    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users,null,2),(err)=>{
-        if(err){console.log('Failed to write into file');
-        }else{console.log('Written into file successfully');}
+    let date=new Date();
+    let info=`${date.getDate()} ${date.getHours()}:${date.getMinutes()}  ${date.getMinutes()} ${date.getSeconds()} ${date.getMilliseconds()} \n`;
+    fs.appendFile('./log.txt',info,(err)=>{
+        if(err){
+            console.log('Failed to write file.',err);
+        } else{
+            console.log('Logged successfully!');
+        }
     });
+    return res.send('Hey! This is the homepage!');
 });
 
-exp.listen(1234,()=>{console.log('Server started!');});
+exp.get('/about',(req,res)=>{
+    return res.send('Hey! This is the about page!');
+});
+
+exp.listen(1234,()=>{console.log('Server started!')});
